005d87f0bf47699bdea486c6468fea3266aa1eb6
codesnajam <contactnajamgujjar@gmail.com>
2022-12-17 17:54:16 +0500

    Fixed Linkedin Icons in Teams Sections

--- src/components/home/early-adopters.tsx
+++ src/components/home/early-adopters.tsx
@@ -13,7 +13,85 @@ const Item = ({ title, info1, info2, position }: any) => (
   </div>
 );
 
-const EarlyAdopters = ({ markdownObject }: any) => {
+
+
+
+
+
+const EarlyAdopters = ({  }: any) => {
+  const [markdownObject, setMarkdownObject] = React.useState({
+    // header
+    headerTitleWithLogoColor: "",
+    headerTitleWithWhiteColor: "",
+    headerComingSoon: "",
+    headerImage: "",
+
+    // works
+    worksTitleWe: "",
+    worksTitleWithLogoColor: "",
+    worksTitleWithWhiteColor: "",
+    worksImage1: "",
+    worksImage1Name: "",
+    worksImage1Position: "",
+    worksImage1Skill: "",
+    worksImage2: "",
+    worksImage2Name: "",
+    worksImage2Position: "",
+    worksImage2Skill: "",
+    worksImage3: "",
+    worksImage3Name: "",
+    worksImage3Position: "",
+    worksImage3Skill: "",
+
+    // bannerBg
+    bannerBgQuote: "",
+    bannerBgName: "",
+    bannerBgPosition: "",
+
+    // brainsBehind
+    brainsBehindTitleWithLogoColor: "",
+    brainsBehindTitleWithWhiteColor: "",
+    bannerBgImage1: "",
+    bannerBgImage2: "",
+    bannerBgImage3: "",
+    bannerBgImage1Name: "",
+    bannerBgImage1Info1: "",
+    bannerBgImage2Name: "",
+    bannerBgImage3Name: "",
+    bannerBgImage3Info1: "",
+    bannerBgImage3Info2: "",
+    bannerBgImage2Info1: "",
+    bannerBgImage3Info3: "",
+    bannerBgImage2Info2: "",
+    bannerBgImage1Info2: "",
+    bannerBgImage2Info3: "",
+    bannerBgImage1Info3: "",
+
+    // sponsorApply
+    sponsorApplyTitleWithWhiteColor1: "",
+    sponsorApplyTitleWithLogoColor: "",
+    sponsorApplyTitleWithWhiteColor2: "",
+    sponsorApplyLogo1: "",
+    sponsorApplyLogo2: "",
+    sponsorApplyBtnText: "",
+
+    // earlyAdopters
+    earlyAdoptersAdopterTitle3Info1: "",
+    earlyAdoptersAdopterTitle2Info1: "",
+    earlyAdoptersAdopterTitle3Info2: "",
+    earlyAdoptersAdopterTitle2Info2: "",
+    earlyAdoptersAdopterTitle1Position: "",
+    earlyAdoptersAdopterTitle1Info1: "",
+    earlyAdoptersAdopterTitle2Position: "",
+    earlyAdoptersAdopterTitle1Info2: "",
+    earlyAdoptersAdopterTitle3Position: "",
+    earlyAdoptersTitleWithLogoColor: "",
+    earlyAdoptersAdopterTitle1: "",
+    earlyAdoptersAdopterTitle2: "",
+    earlyAdoptersAdopterTitle3: "",
+    earlyAdoptersTitleWithWhiteColor: "",
+  });
+
   return (
     <div className="early-adopters">
       <h1 className="early-adopters-header">
